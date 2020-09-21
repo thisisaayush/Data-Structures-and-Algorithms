77072dfdd4561299c2e667c9705a2dafa98883e1
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-09-20 23:55:51 -0500

    Add files via upload

---
+++ Python/anagramcheck.py
@@ -0,0 +1,19 @@
+def anagram_check(s1,s2):
+    a_list1 = list(s1)
+    a_list2 = list(s2)
+
+    a_list1.sort()
+    a_list2.sort()
+
+    index = 0
+    flag = True
+
+    while index < len(s1) and flag:
+        if a_list1[index] == a_list2[index]:
+            index = index + 1
+        else:
+            flag = False
+
+    return flag
+
+
