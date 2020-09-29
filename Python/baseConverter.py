2f3dae6a308b2aac8b6c9764b528d487455d44bc
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-09-28 21:37:06 -0500

    Add files via upload

---
+++ Python/baseConverter.py
@@ -0,0 +1,16 @@
+def baseConverter(decNumber, base):
+    digits = "0123456789ABCDEF"
+    toBase = Stack()
+    while decNumber > 0:
+        rem = decNumber % base
+        toBase = rem.push()
+        decNumber = decNumber / base
+
+    result = ""
+
+    while not toBase.isEmpty():
+        result = result + digits[toBase.pop()]
+
+    return result
+
+
