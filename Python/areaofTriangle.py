bc8c47f9b7c19887729d4933a7ea829d9d1584ee
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-10-25 17:33:14 -0500

    Update areaofTriangle.py

--- Python/areaofTriangle.py
+++ Python/areaofTriangle.py
@@ -1,7 +1,7 @@
 def area(base,height):
     return (base*height/2)
-
-
+#The variable a and b will calculate the area of a triangle of the entered parameter value and 
+#sum variable will sum the area of a and b and finally prints the sum.
 a = area(5,6)
 b = area(4,5)
 sum = a + b
