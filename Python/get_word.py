34cd205ca26fee14fcd099c12dacfbb66f39b78b
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-10-29 23:45:36 -0500

    Add files via upload

---
+++ Python/get_word.py
@@ -0,0 +1,11 @@
+def get_word(sentence, n):
+	# Only proceed if n is positive
+	if n > 0:
+		words = sentence.split()
+		# Only proceed if n is not more than the number of words
+		if n <= len(words):
+			return words[n - 1]
+	return("")
+
+print(get_word("This is a lesson about lists", 4)) # Should print: lesson
+print(get_word("This is a lesson about lists", -4)) # Nothing
