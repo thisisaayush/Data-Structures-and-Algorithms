3a21c4488c41813a2c05a4164641a63f1f9f3278
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-11-01 14:38:12 -0600

    Add files via upload

---
+++ Python/octal_to_string.py
@@ -0,0 +1,19 @@
+#didn't understand
+def octal_to_string(octal):
+    result = ""
+    value_letters = [(4, "r"), (2, "w"), (1, "x")]
+    # Iterate over each of the digits in octal
+    for i in [int(n) for n in str(octal)]:
+        # Check for each of the permissions values
+        for value, letter in value_letters:
+            if i >= value:
+                result += letter
+                i -= value
+            else:
+                result = result + "-"
+    return result
+
+
+print(octal_to_string(755))  # Should be rwxr-xr-x
+print(octal_to_string(644))  # Should be rw-r--r--
+print(octal_to_string(750))  # Should be rwxr-x---
