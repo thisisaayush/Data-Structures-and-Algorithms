a6248544897131620e030cc97ac73580c499c1e2
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-11-11 17:32:08 -0600

    Add files via upload

---
+++ Python/highlight_word.py
@@ -0,0 +1,5 @@
+def highlight_word(sentence, word):
+	return(sentence.replace(word,word.upper()))
+
+print(highlight_word("Have a nice day", "nice"))
+print(highlight_word("Shhh, don't be so loud!", "loud"))
