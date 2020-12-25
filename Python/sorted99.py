4d9c4445a20c5a35fc21a4777459ef8e2d28b63f
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-12-24 19:25:19 -0600

    Add files via upload

---
+++ Python/sorted99.py
@@ -0,0 +1,7 @@
+numbers = [2.1,6.5,9.1,4.5,3.7]
+new_numbers = sorted(numbers)
+print(new_numbers)
+
+letters = "The quick brown fox jumps over the lazy dog."
+x = sorted(letters, key = str.casefold)
+print(x)
