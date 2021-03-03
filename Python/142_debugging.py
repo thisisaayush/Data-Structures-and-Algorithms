da2166a49f61c872a785f8d4dba76a120fcf0486
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2021-03-03 13:32:06 -0600

    Add files via upload

---
+++ Python/142_debugging.py
@@ -0,0 +1,14 @@
+def divide(x,y):
+    result = x / y
+    return result
+
+answer = divide(10,2)
+print(answer)
+
+sec_ans = divide(15,2)
+print(sec_ans)
+
+for value in range(1, 101):
+    outcome = divide(value,2)
+    print(outcome)
+
