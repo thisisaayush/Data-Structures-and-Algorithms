f869ff6f219568f8e10c811ddee566b14bc4a473
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2021-06-06 21:05:20 -0500

    Add files via upload

---
+++ Data Structures and ALgorithm/S93_search_linkedlist.py
@@ -0,0 +1,15 @@
+"""
+def search(key):
+    p = self._head
+    index = 0
+
+    while p:
+        if p._element == key:
+            return index
+
+        p = p._next
+        index = index + 1
+
+    return -1
+
+"""
